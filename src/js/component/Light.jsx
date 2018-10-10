77eb6bb04165aeafa2ba6a582a28198336f8a0a2
Blake Albertson <albertson.blake@yahoo.com>
2018-10-10 19:35:32 -0400

    finish project

--- src/js/component/Light.jsx
+++ src/js/component/Light.jsx
@@ -1,14 +1,40 @@
 import React from "react";
+import { render } from "react-dom";
 
-export const Light = () => {
-	return (
-		<div>
-			<div className="stick" />
-			<div className="light">
-				<div className="red" />
-				<div className="yellow" />
-				<div className="green" />
+export class Light extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			active: null
+		};
+	}
+
+	render() {
+		let redActive = "";
+		if (this.state.active == "red") redActive = "glow";
+		let yellowActive = "";
+		if (this.state.active == "yellow") yellowActive = "glow";
+		let greenActive = "";
+		if (this.state.active == "green") greenActive = "glow";
+
+		return (
+			<div>
+				<div className="stick" />
+				<div className="light">
+					<div
+						className={"red " + redActive}
+						onClick={() => this.setState({ active: "red" })}
+					/>
+					<div
+						className={"yellow " + yellowActive}
+						onClick={() => this.setState({ active: "yellow" })}
+					/>
+					<div
+						className={"green " + greenActive}
+						onClick={() => this.setState({ active: "green" })}
+					/>
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
